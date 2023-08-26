5a53647fbf8966a6f1d9e2d45219f2f3af3d920c
Nadiiameyer <nadia.suworowa@hotmail.com>
2023-08-26 15:31:55 +0200

    Initial commit

---
+++ src/MyClass.java
@@ -0,0 +1,16 @@
+public class MyClass {
+
+    class myClass {
+        int Year = 2023;
+
+        public static int MyMETHOD(int a, int b) {
+            int c = a + b; //sum of 2 numbers
+            return c;
+        }
+
+        public static void main(String[] args) {//can't understand why statics are red :)
+            MyMETHOD(1991, 32);
+
+        }
+    }
+}
